58379fb645d41fa3172e5233f25d20071233f642
Anson Wong <270021714@cit3-501m-14.local>
2019-09-03 15:30:35 +1200

    app setup completed

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+import {Router, Link, navigate} from '@reach/router'
+
+import './App.css';
+import api from './api'
+
+class App extends Component{
+  constructor(props)
+  super(props){
+    this.state ={
+      currentUser:{},
+    }
+  }
+handleLogOut=()=>{
+
+}
+componentDidMount=()=>{
+
+}
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default App;
