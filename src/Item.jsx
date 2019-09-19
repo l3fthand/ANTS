6702e4e83575f4c7ee545f559462d9170731acd5
Sam Robertson <270020120@cit3-102m-11.acg.ac.nz>
2019-09-19 13:57:08 +1200

    added purchased filer

--- src/Item.jsx
+++ src/Item.jsx
@@ -9,6 +9,9 @@ import './App.css';
 class Item extends Component{
   constructor(props){
     super(props);
+      this.state = {
+        purchaser_id:null,
+    }
   }
   // routeGetProduct = (id) => {
   //   api.getProduct(id).then(res => this.setState({product:res.data}))
@@ -16,6 +19,11 @@ class Item extends Component{
   deleteProduct = () => {
     var {id, refreshData} = this.props;
     api.deleteProduct(id).then(() => refreshData())
+  }
+
+  componentDidMount(){
+    var {purchaser_id} = this.props;
+    this.setState({purchaser_id:purchaser_id})
   }
 
 
@@ -26,16 +34,17 @@ class Item extends Component{
       
 
 
-      <Col>
-        <Card>
-        <Link to={'/products/'+id}><Card.Img variant="top" src={server+photo}/></Link>
-            <Card.Body>
-                <Card.Title><Link to={'/products/'+id}>{name}</Link>
-                </Card.Title>
-
-            </Card.Body>
-        </Card>
-      </Col>
+      this.state.purchaser_id ? null : (
+        <Col>
+          <Card>
+          <Link to={'/products/'+id}><Card.Img variant="top" src={server+photo}/></Link>
+              <Card.Body>
+                  <Card.Title><Link to={'/products/'+id}>{name}</Link>
+                  </Card.Title>
+  
+              </Card.Body>
+          </Card>
+        </Col>)
 
   // <div className="Item">
   //     <Card
