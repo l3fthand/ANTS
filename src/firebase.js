b9d04b98095b2a5278c8fa0c0941679ffa5e97ff
NidhiPatel <nidhi.patel@alphacertlabs.com>
2020-07-30 14:10:48 +1200

    final commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+ import * as firebase from "firebase";
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBL7cMZUq6m_6jSLJV0-S47PPn0ORdZTRo",
+    authDomain: "ants-361ea.firebaseapp.com",
+    databaseURL: "https://ants-361ea.firebaseio.com",
+    projectId: "ants-361ea",
+    storageBucket: "ants-361ea.appspot.com",
+    messagingSenderId: "899476659689",
+    appId: "1:899476659689:web:d5af6b4b2ffb865886ff78"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
